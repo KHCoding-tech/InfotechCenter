7a2ed9c0ceb38a3ae75313c888ac891e70dd42b0
KHCoding-tech <konnerhunt81@gmail.com>
2025-02-17 14:00:26 -0500

    Added Elif and Else to gasLevelAlert() - GitHub

--- BlackWidow.py
+++ BlackWidow.py
@@ -21,12 +21,17 @@ def gasLevelAlert():
         sleep(1.25)
         print("Calling AAA")
     elif gasLevelIndicator == "Low":
-        print("Your gas tank is extremely low, checking GPS for the closest gas station\n")
+        print("Your gas tank is extremely LOW, checking GPS for the closest gas station.\n")
         sleep(1.25)
         print("The closest gas station is", gasStations(), "which is",milesToGasStationLow, "miles away.")
     elif gasLevelIndicator == "Quarter Tank":
-        print("Your gas tank is on a quarter tank, checking GPS for the closest gas station\n")
+        print("Your gas tank is on a QUARTER TANK, checking GPS for the closest gas station.\n")
         sleep(1.25)
         print("The closest gas station is", gasStations(), "which is",milesToGasStationQuarterTank, "miles away.")
-
+    elif gasLevelIndicator == "Half Tank":
+        print("Your gas tank is on a HALF OF A TANK, which is enough to get to your destination.\n")
+    elif gasLevelIndicator == "Three Quarter Tank":
+        print("Your gas tank is on THREE QUARTER OF A TANK FULL.\n")
+    else:
+        print("Your gas tank is FULL.")
 gasLevelAlert()
